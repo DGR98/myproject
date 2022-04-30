b12def36167009a2c3047937ddf155ef1a3df9b4
DGR98 <55596823+DGR98@users.noreply.github.com>
2022-05-01 02:31:45 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,4 +1,9 @@
 echo "printing 10 lines"
 for i in `seq 10`
+kubeadm join 10.0.2.15:6443 --token bigwet.hmryeuvj4x10yvat \
+	--discovery-token-ca-cert-hash sha256:7e042f5a72e7bd7155867d4be25f46c3c753f08cc969d661b3a5a41870b086bb
+
+
+
 
 
